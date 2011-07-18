bc395a4afa08c1379c0621d6409648c8d9a1bd30
Endy Muhardin <endy.muhardin@gmail.com>
2011-07-18 10:57:35 +0700

    domain class

---
+++ src/main/java/belajar/spring/domain/Address.java
@@ -0,0 +1,27 @@
+package belajar.spring.domain;
+
+public class Address {
+	private String jalan;
+	private String kota;
+	private String kodepos;
+	public String getJalan() {
+		return jalan;
+	}
+	public void setJalan(String jalan) {
+		this.jalan = jalan;
+	}
+	public String getKota() {
+		return kota;
+	}
+	public void setKota(String kota) {
+		this.kota = kota;
+	}
+	public String getKodepos() {
+		return kodepos;
+	}
+	public void setKodepos(String kodepos) {
+		this.kodepos = kodepos;
+	}
+	
+	
+}
---
+++ src/main/java/belajar/spring/domain/Person.java
@@ -0,0 +1,29 @@
+package belajar.spring.domain;
+
+import java.util.Date;
+
+public class Person {
+	private Integer id;
+	private String nama;
+	private Date tanggalLahir;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public Date getTanggalLahir() {
+		return tanggalLahir;
+	}
+	public void setTanggalLahir(Date tanggalLahir) {
+		this.tanggalLahir = tanggalLahir;
+	}
+	
+	
+}
