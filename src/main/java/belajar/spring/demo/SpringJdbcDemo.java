3a77759a3bd78a5689ba73c2f40a23ef6fa3d040
Endy Muhardin <endy.muhardin@gmail.com>
2011-07-19 10:11:41 +0700

    main class untuk menjalankan demo spring jdbc

---
+++ src/main/java/belajar/spring/demo/SpringJdbcDemo.java
@@ -0,0 +1,30 @@
+package belajar.spring.demo;
+
+import java.util.Date;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import belajar.spring.domain.Person;
+import belajar.spring.jdbc.PersonDao;
+
+public class SpringJdbcDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ApplicationContext ctx = 
+				new ClassPathXmlApplicationContext("classpath:jdbc.xml");
+		
+		
+		PersonDao personDao = (PersonDao) ctx.getBean("personDao");
+		
+		Person p = new Person();
+		p.setNama("Endy Muhardin");
+		p.setTanggalLahir(new Date());
+		
+		personDao.save(p);
+	}
+
+}
